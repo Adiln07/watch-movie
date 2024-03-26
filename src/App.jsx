fa3b3639a19babfcf8fd14e07432aed8b8973d1e
Adiln07 <adil.nusabakti117@gmail.com>
2024-03-26 23:07:42 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>Watch Movie</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
